c7d1d8dc06193804c51a1838cdfc899ade5f0164
Sagar Kunwar Chhetri <36431491+sagarkunwar24@users.noreply.github.com>
2019-03-11 11:30:01 -0400

    Create prob12_6.py

---
+++ prob12_6.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2.7
+# initiate a few variables
+i = 0
+x = []
+y = []
+
+# read csv file and store data in arrays x and y
+import csv
+with open('12_4.csv', 'rb') as csvfile:
+   reader = csv.reader(csvfile)
+   for row in reader:
+       i += 1
+       print i
+       xy = row
+       x.append(int(xy[0]))
+       y.append(int(xy[1]))
+       print x
+       print y
+
+print ("x=")
+print ("y=")
+
+# now use arrays x and y to run linear regression
+import scipy.stats as stats
+
+lr = stats.linregress
+slope, intercept, rvalue, pvalue, stderr = lr(x,y)
+
+# print results to screen
+#print ("slope, intercept, rvalue, pvalue, stderr")
+print ("slope, intercept, r-squared")
+print slope
+print intercept
+print rvalue**2
+#print pvalue
+#print stderr
+#print Sagar Kunwar
